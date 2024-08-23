015750df8e1ada6a495c80f47f032b29d4f51f0a
Wendy Liang <wendyl.y.123@gmail.com>
2024-08-23 04:03:44 -0700

    update all imgs and locations

--- index.js
+++ index.js
@@ -1,1 +1,462 @@
 // Studio script
+
+/* Japan Script */
+let itinerary = {
+  2: [
+    {
+      lat: 47.448365,
+      long:  -122.308593,
+      imgUrl: "imgs/seatac.jpg",
+      descrip: "3/2 4:08AM Departing from SeaTac Airport"
+    },
+  ],
+  3: [
+    {
+      lat: 35.5493932,
+      long: 139.7772637,
+      imgUrl: "imgs/haneda.jpg",
+      descrip: "3/3 8:37PM Arrived in Haneda Airport"
+    },
+    {
+      lat: 35.6957513,
+      long: 139.6980145,
+      imgUrl: "imgs/kabukicho.jpg",
+      descrip: "3/3 9:59PM Dropped off at Tokyu Kabukicho Tower via Airport Shuttle"
+    },
+    {
+      lat: 35.7019034,
+      long: 139.699137,
+      imgUrl: "imgs/yoshinoya.jpg",
+      descrip: "3/3 10:43PM First meal in Japan! Yoshinoya ftw."
+    },
+  ],
+  4: [
+    {
+      lat: 35.695799,
+      long: 139.701528,
+      imgUrl: "imgs/kuromi.jpg",
+      descrip: "3/4 12:33AM First plushie of the trip won!"
+    },
+    {
+      lat: 35.701603,
+      long: 139.698763,
+      imgUrl: "imgs/4.1.jpg",
+      descrip: "3/4 8:58AM First selfie of the Japan trip!"
+    },
+    {
+      lat: 35.644429,
+      long: 139.707087,
+      imgUrl: "imgs/4.2.jpg",
+      descrip: "3/4 12:35PM Got a perm at Mint Salon"
+    },
+    {
+      lat: 35.668863,
+      long: 139.768724,
+      imgUrl: "imgs/4.3.jpg",
+      descrip: "3/4 5:50PM Tried out fancy coffee at Glitch"
+    }
+  ],
+  5: [
+    {
+      lat: 35.701737,
+      long: 139.698594,
+      imgUrl: "imgs/5.1.jpg",
+      descrip: "3/5 9:31AM Can't start the day without some vending machine drinks"
+    },
+    {
+      lat: 35.678785,
+      long: 139.762788,
+      imgUrl: "imgs/5.2.jpg",
+      descrip: "3/5 2:05PM Omakase for $50"
+    },
+    {
+      lat: 35.644429,
+      long: 139.707087,
+      imgUrl: "imgs/5.3.jpg",
+      descrip: "3/5 2:59PM Daily Coffee"
+    },
+    {
+      lat: 35.701296,
+      long: 139.771158,
+      imgUrl: "imgs/5.4.jpg",
+      descrip: "3/5 7:08PM Curryyyy!"
+    },
+  ],
+  6: [
+    {
+      lat: 35.699852,
+      long: 139.700549,
+      imgUrl: "imgs/6.1.jpg",
+      descrip: "3/6 9:25AM Famichiki was a must every day"
+    },
+    {
+      lat: 35.626576,
+      long: 139.888010,
+      imgUrl: "imgs/6.2.jpg",
+      descrip: "3/6 11:34AM Tokyo DisneySea"
+    },
+    {
+      lat: 35.693517,
+      long: 139.701361,
+      imgUrl: "imgs/6.3.jpg",
+      descrip: "3/6 10:44PM Torikizoku to end the night"
+    },
+  ],
+  7: [
+    {
+      lat: 35.694561,
+      long: 139.701775,
+      imgUrl: "imgs/7.1.jpg",
+      descrip: "3/7 12:45PM Curry from CoCo"
+    },
+    {
+      lat: 35.696644,
+      long: 139.570040,
+      imgUrl: "imgs/7.2.jpg",
+      descrip: "3/7 2:14PM Ghibli Museum"
+    },
+    {
+      lat: 35.704615,
+      long: 139.574524,
+      imgUrl: "imgs/7.3.jpg",
+      descrip: "3/7 5:27PM Matcha roll and some matcha lattes"
+    },
+  ],
+  8: [
+    {
+      lat: 35.645704,
+      long: 139.783227,
+      imgUrl: "imgs/8.1.jpg",
+      descrip: "3/8 12:36PM Had the best sashimi at the Toyosu Fish Market"
+    },
+    {
+      lat: 35.730888,
+      long: 139.706736,
+      imgUrl: "imgs/8.2.jpg",
+      descrip: "3/8 4:48PM Went to an owl cafe, they even had a python and porcupines"
+    },
+    {
+      lat: 35.729298,
+      long: 139.719169,
+      imgUrl: "imgs/8.3.jpg",
+      descrip: "3/8 7:44PM Pokemon Center at Sunshine City"
+    },
+  ],
+  9: [
+    {
+      lat: 35.694395,
+      long: 139.701619,
+      imgUrl: "imgs/9.1.jpg",
+      descrip: "3/9 11:46AM Finally tried out Ichiran Ramen"
+    },
+    {
+      lat: 35.730888,
+      long: 139.706736,
+      imgUrl: "imgs/9.2.jpg",
+      descrip: "3/9 5:02PM Tea & Spoon Nanaya Aoyama"
+    },
+    {
+      lat: 35.669673,
+      long: 139.705837,
+      imgUrl: "imgs/9.3.jpg",
+      descrip: "3/9 8:36PM A hole in the wall"
+    },
+  ],
+  10: [
+    {
+      lat: 35.695198,
+      long: 139.701317,
+      imgUrl: "imgs/10.1.jpg",
+      descrip: "3/10 9:54AM Spent the night at a manga cafe and had corn soup plus ice cream for breakfast"
+    },
+    {
+      lat: 35.713274,
+      long: 139.777432,
+      imgUrl: "imgs/10.2.jpg",
+      descrip: "3/10 12:57PM Getting ready to take the bullet train to an onsen"
+    },
+    {
+      lat: 36.847963,
+      long: 139.046910,
+      imgUrl: "imgs/10.3.jpg",
+      descrip: "3/10 6:02PM Kaiseki style dinner at the onsen - was super good"
+    },
+    {
+      lat: 36.847963,
+      long: 139.046910,
+      imgUrl: "imgs/10.4.jpg",
+      descrip: "3/10 11:59PM Vending machine ice cream to finish the night"
+    },
+  ],
+  11: [
+    {
+      lat: 36.848071,
+      long: 139.046882,
+      imgUrl: "imgs/11.1.jpg",
+      descrip: "3/11 8:00AM Traditional breakfast at the onsen"
+    },
+    {
+      lat: 34.985543,
+      long: 135.760125,
+      imgUrl: "imgs/11.2.jpg",
+      descrip: "3/11 2:51PM Kyoto here we are!"
+    },
+    {
+      lat: 35.004999,
+      long: 135.762522,
+      imgUrl: "imgs/11.3.jpg",
+      descrip: "3/11 5:01PM First stop - Nishiki Market"
+    },
+    {
+      lat: 36.847963,
+      long: 139.046910,
+      imgUrl: "imgs/11.3.jpg",
+      descrip: "3/11 7:28PM Gyukatsu - self grill meat"
+    },
+  ],
+  12: [
+    {
+      lat: 35.017613,
+      long: 135.760503,
+      imgUrl: "imgs/12.1.jpg",
+      descrip: "3/12 9:04AM Starting off the morning with a daily konbini run"
+    },
+    {
+      lat: 34.968703,
+      long: 135.785832,
+      imgUrl: "imgs/12.2.jpg",
+      descrip: "3/12 11:06AM Fushimi Inari Taisha - too bad it was raining that day"
+    },
+    {
+      lat: 34.890427,
+      long: 135.801057,
+      imgUrl: "imgs/12.3.jpg",
+      descrip: "3/12 4:19PM Uji - city of matcha!"
+    },
+    {
+      lat: 35.005660,
+      long: 135.769292,
+      imgUrl: "imgs/12.4.jpg",
+      descrip: "3/12 7:33PM First time trying sukiyaki"
+    },
+  ],
+  13: [    
+    {
+      lat: 34.665332,
+      long: 135.505868,
+      imgUrl: "imgs/13.1.jpg",
+      descrip: "3/13 1:54PM Starting the afternoon at Kuromon Market"
+    },
+    {
+      lat: 34.668707,
+      long: 135.503260,
+      imgUrl: "imgs/13.2.jpg",
+      descrip: "3/13 4:41PM Even sewer covers are so pretty"
+    },
+    {
+      lat: 34.669085,
+      long: 135.501412,
+      imgUrl: "imgs/13.3.jpg",
+      descrip: "3/13 9:10PM Dotonburi - super busy even at night"
+    },
+    {
+      lat: 35.005660,
+      long: 135.769292,
+      imgUrl: "imgs/13.4.jpg",
+      descrip: "3/13 9:46PM Fried skewers and drinks"
+    },
+  ],
+  14: [
+    {
+      lat: 34.667256,
+      long: 135.430465,
+      imgUrl: "imgs/14.1.jpg",
+      descrip: "3/14 7:31AM Nintendo World first thing in the morning to avoid lines"
+    },
+    {
+      lat: 34.667916,
+      long: 135.429935,
+      imgUrl: "imgs/14.2.jpg",
+      descrip: "3/14 10:19AM First meal of the day"
+    },
+    {
+      lat: 34.665376,
+      long: 135.434392,
+      imgUrl: "imgs/14.3.jpg",
+      descrip: "3/14 3:02PM Hello Kitty Bun at Universal Studios"
+    },
+    {
+      lat: 34.666751,
+      long: 135.501384,
+      imgUrl: "imgs/14.4.jpg",
+      descrip: "3/14 10:07PM Torikizoku to end the night"
+    },
+  ],
+  15: [
+    {
+      lat: 34.666402,
+      long: 135.504972,
+      imgUrl: "imgs/15.1.jpg",
+      descrip: "3/15 1:25PM Tried out Japan McDonald's ebi burger - 10/10"
+    },
+    {
+      lat: 34.673802,
+      long: 135.500808,
+      imgUrl: "imgs/15.2.jpg",
+      descrip: "3/15 7:46PM Totoro spotted"
+    },
+    {
+      lat: 34.669258,
+      long: 135.508995,
+      imgUrl: "imgs/15.3.jpg",
+      descrip: "3/15 9:17PM First okonomiyaki experience"
+    },
+    {
+      lat: 34.669257,
+      long: 135.509041,
+      imgUrl: "imgs/15.4.jpg",
+      descrip: "3/15 10:17PM Melopan ice cream sandwich - it was a little too much"
+    },
+  ],
+  16: [
+    {
+      lat: 34.733806,
+      long: 135.501537,
+      imgUrl: "imgs/16.1.jpg",
+      descrip: "3/16 11:55AM Waiting for our next bullet train while eating some fumichiki"
+    },
+    {
+      lat: 35.671203,
+      long: 139.761371,
+      imgUrl: "imgs/16.2.jpg",
+      descrip: "3/16 7:12PM Ramen with pork broth"
+    },
+    {
+      lat: 35.671558,
+      long: 139.764636,
+      imgUrl: "imgs/16.3.jpg",
+      descrip: "3/16 7:54PM Manneken waffles"
+    },
+  ],
+  17: [
+    {
+      lat: 35.713735,
+      long: 139.793268,
+      imgUrl: "imgs/17.1.jpg",
+      descrip: "3/17 12:28PM don don don donki - donki quihote~"
+    },
+    {
+      lat: 35.713107,
+      long: 139.788533,
+      imgUrl: "imgs/17.2.jpg",
+      descrip: "3/17 3:09PM Knife so pretty - price not so much at Kappabashi street"
+    },
+    {
+      lat: 35.713248,
+      long: 139.796533,
+      imgUrl: "imgs/17.3.jpg",
+      descrip: "3/17 4:34PM Senso-ji temple visit except with bad fortune"
+    },
+    {
+      lat: 35.698590,
+      long: 139.706851,
+      imgUrl: "imgs/17.4.jpg",
+      descrip: "3/17 11:59PM Last night in Japan, it's kbbq time except with charcoal because we couldn't read the menu..."
+    },
+  ],
+  18: [
+    {
+      lat: 35.708220,
+      long: 139.790835,
+      imgUrl: "imgs/18.1.jpg",
+      descrip: "3/18 9:50AM Last famichiki run :("
+    },
+    {
+      lat: 35.447173,
+      long: 139.654523,
+      imgUrl: "imgs/18.2.jpg",
+      descrip: "3/18 1:52PM With our last hours in Japan, we took an hour and went to Yokohama for the Gundam Factory"
+    },
+    {
+      lat: 35.698138,
+      long: 139.701919,
+      imgUrl: "imgs/18.3.jpg",
+      descrip: "3/18 4:40PM Last meal in Japan - about to miss the airport shuttle..."
+    },
+    {
+      lat: 35.548985,
+      long: 139.769106,
+      imgUrl: "imgs/18.4.jpg",
+      descrip: "3/18 9:14PM Time to fly back to SeaTac"
+    },
+  ],
+};
+
+// Initialize map to Tokyo, Japan
+const map = L.map('map').setView([35.670202,139.7564271], 11);
+
+L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+  maxZoom: 20,
+  subdomains:['mt0','mt1','mt2','mt3']
+}).addTo(map);
+
+let markers = [];
+const addPintoMap = (lat, long, imgUrl = '', popupText = '') => {
+  const popup = `<img class="popup" src="${imgUrl}"><br>${popupText}`;
+  let marker = L.marker([lat,long], { autoPanOnFocus: true }).addTo(map);
+  marker.bindPopup(popup, {
+    autoPan: true,
+    autoPanPadding: [50,50],
+  });
+  markers.push(marker);
+};
+
+const addPintoMapOnClick = (day = 2) => {
+  if (day === 2) {
+    // SeaTac Airport
+    map.setView([47.448365, -122.308593], 11);
+  } else if (day < 8) {
+    // Tokyo
+    map.setView([35.670202,139.7564271], 11);
+  } else if (day < 11) {
+    // Tokyo
+    map.setView([35.6914962,139.6811979], 11);
+  } else if (day === 11){
+    // Onsen
+    map.setView([36.8479785,139.0442163], 11);
+  } else if (day < 16) {
+    // Kyoto and Osaka
+    map.setView([34.8442614,135.5627794], 10);
+  } else {
+    // Tokyo
+    map.setView([35.670202,139.7564271], 11);
+  }
+  // If markers already exist, remove from map first
+  if (markers) {
+    for (let marker of markers) marker.remove();
+    markers = [];
+  }
+
+  for (let event of itinerary[day]) {
+    addPintoMap(event.lat, event.long, event.imgUrl, event.descrip);
+  }    
+};
+
+// Render map with corresponding markers for day clicked
+const onHighlightDayClick = (event) => {
+  const elem = event.target;
+  addPintoMapOnClick(parseInt(elem.innerText));
+  // Remove selected style on preview clicked date
+  if (document.querySelector('.selected')) {
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  elem.classList.add('selected');
+  document.getElementById('map').classList.add('show');
+  map.invalidateSize(); // Rerenders/update map in case container has changed
+}
+
+// Put a click event listener on all the highlighted days
+const highlightDaysElems = [...document.getElementsByClassName('highlight')];
+highlightDaysElems.forEach(dayElem => {
+  dayElem.addEventListener('click', onHighlightDayClick);
+});
